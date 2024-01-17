da51f03a7718440cc185b6955d48f39dc7d08e18
EruelUrsua <eruelursua@gmail.com>
2024-01-17 15:35:45 +0800

    added patient buttom nav

---
+++ lib/nav.dart
@@ -0,0 +1,50 @@
+import 'dart:ffi';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class NavigationMenu extends StatefulWidget {
+  const NavigationMenu({Key? key}) : super(key: key);
+
+  @override
+  State<NavigationMenu> createState() => _NavigationMenuState();
+}
+
+class _NavigationMenuState extends State<NavigationMenu> {
+  int _currentIndex = 0;
+  List<Widget> body = const [
+    Text('Home'),
+    Text('Booking'),
+    Text('Profile'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: body[_currentIndex],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (int newIndex) {
+            setState(() {
+              _currentIndex = newIndex;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: 'Book',
+              icon: Icon(Icons.bookmark_rounded),
+            ),
+            BottomNavigationBarItem(
+              label: 'Profile',
+              icon: Icon(Icons.person),
+            ),
+          ],
+        ));
+  }
+}
