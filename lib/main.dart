d185caa58c2bee1363c457c105513fa0c704f232
EruelUrsua <eruelursua@gmail.com>
2024-01-30 21:02:15 +0800

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:solcha2024/nav.dart';
 import 'package:solcha2024/profile.dart';
 import 'package:solcha2024/users/signup.dart';
 import 'package:solcha2024/users/startup.dart';
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,8 +31,26 @@ class MyApp extends StatelessWidget {
       ),
       home:
           NavigationMenu(), //<------------------ change the class if you need to test
-          // Welcome(),
+      // Welcome(),
       //Profile(),
+      //     FlutterSplashScreen.fadeIn(
+      //   backgroundColor: Colors.white,
+      //   onInit: () {
+      //     debugPrint("On Init");
+      //   },
+      //   onEnd: () {
+      //     debugPrint("On End");
+      //   },
+      //   childWidget: SizedBox(
+      //     height: 200,
+      //     width: 200,
+      //     child: Image.asset("assets/logo.png"),
+      //   ),
+      //   duration: const Duration(milliseconds: 1500),
+      //   animationDuration: const Duration(milliseconds: 1000),
+      //   onAnimationEnd: () => debugPrint("On Fade In End"),
+      //   nextScreen: const NavigationMenu(),
+      // ),
     );
   }
 }
