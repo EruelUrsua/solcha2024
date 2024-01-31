f5f09ff4b00e75d6d0d7f40fae20d8078120f60a
raelactao <raevictoria.lactao@benilde.edu.ph>
2024-01-31 23:45:31 +0800

    added curve and searchbar

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:solcha2024/custom_appbar_shape.dart';
 import 'package:solcha2024/toast.dart';
 import 'package:solcha2024/users/login.dart';
 
@@ -9,23 +11,73 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:
-          AppBar(backgroundColor: const Color(0xFF8E8BE6), actions: <Widget>[
-        IconButton(
-          icon: const Icon(Icons.notifications),
-          color: Colors.grey.shade800,
-          onPressed: () {
-            ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('Should open something')));
-          },
-        ),
+      appBar: AppBar(
+          toolbarHeight: 130,
+          title: Center(
+              child: Text(
+            "Hello username!",
+            style: TextStyle(
+              color: Color(0xFF392C82),
+              fontSize: 17,
+            ),
+          )),
+          backgroundColor: const Color(0xFFdcdcf4),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.notifications),
+              iconSize: 30,
+              color: Colors.grey.shade900,
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Should open something')));
+              },
+            ),
+          ]),
+      body: Column(children: [
+        buildRoundAppBar(context),
+        buildHomeBody(context),
       ]),
-      body: buildHomeBody(context),
-      drawer: Drawer(
-        backgroundColor: Colors.grey.shade50,
+      drawer: const Drawer(
+        backgroundColor: Colors.white,
       ),
     );
   }
+
+  Widget buildRoundAppBar(BuildContext context) => Stack(children: <Widget>[
+        ClipPath(
+          clipper: CustomAppBarShape(),
+          child: Container(
+            height: 120,
+            width: MediaQuery.of(context).size.width,
+            color: const Color(0xFFdcdcf4),
+            //color: Colors.transparent,
+          ),
+        ),
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * .05,
+              width: MediaQuery.of(context).size.width * .90,
+              child: Material(
+                elevation: 8,
+                borderRadius: BorderRadius.circular(30),
+                child: TextField(
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        labelText: "Search",
+                        prefixIcon:
+                            Icon(color: Colors.grey.shade600, Icons.search),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                          borderSide: BorderSide(color: Color(0xFF8E8BE6)),
+                        ))),
+              ),
+            ),
+          ),
+        ),
+      ]);
 
   Widget buildHomeBody(BuildContext context) => Container(
         alignment: Alignment.center,
