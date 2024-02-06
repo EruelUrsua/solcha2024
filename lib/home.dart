e7430c1741f004ab1dac7809e0fbd1221e4aeee4
raelactao <raevictoria.lactao@benilde.edu.ph>
2024-02-06 23:04:22 +0800

    added carousel images to home dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,32 +1,41 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:solcha2024/custom_appbar_shape.dart';
 import 'package:solcha2024/toast.dart';
 import 'package:solcha2024/users/login.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  CarouselController _carouselController = CarouselController();
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          toolbarHeight: 130,
+          iconTheme: IconThemeData(color: Colors.white),
+          toolbarHeight: 80,
           title: Center(
               child: Text(
             "Hello username!",
             style: TextStyle(
-              color: Color(0xFF392C82),
+              color: Colors.white,
               fontSize: 17,
+              fontWeight: FontWeight.bold,
             ),
           )),
-          backgroundColor: const Color(0xFFdcdcf4),
+          backgroundColor: const Color(0xFF8E8BE6),
           actions: <Widget>[
             IconButton(
               icon: const Icon(Icons.notifications),
               iconSize: 30,
-              color: Colors.grey.shade900,
+              //color: Colors.white,
               onPressed: () {
                 ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(content: Text('Should open something')));
@@ -35,6 +44,7 @@ class Home extends StatelessWidget {
           ]),
       body: Column(children: [
         buildRoundAppBar(context),
+        _buildCarousel(),
         buildHomeBody(context),
       ]),
       drawer: const Drawer(
@@ -47,37 +57,75 @@ class Home extends StatelessWidget {
         ClipPath(
           clipper: CustomAppBarShape(),
           child: Container(
-            height: 120,
+            height: 110,
             width: MediaQuery.of(context).size.width,
-            color: const Color(0xFFdcdcf4),
+            color: const Color(0xFF8E8BE6),
             //color: Colors.transparent,
           ),
         ),
+        // search bar
         Center(
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: EdgeInsets.fromLTRB(0.0, 4.0, 0.0, 0.0),
             child: SizedBox(
-              height: MediaQuery.of(context).size.height * .05,
+              height: MediaQuery.of(context).size.height * .04,
               width: MediaQuery.of(context).size.width * .90,
               child: Material(
                 elevation: 8,
                 borderRadius: BorderRadius.circular(30),
                 child: TextField(
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        labelText: "Search",
-                        prefixIcon:
-                            Icon(color: Colors.grey.shade600, Icons.search),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: BorderSide(color: Color(0xFF8E8BE6)),
-                        ))),
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: "Search",
+                      prefixIcon:
+                          Icon(color: Colors.grey.shade700, Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        borderSide: BorderSide(color: Color(0xFF8E8BE6)),
+                      )),
+                  textAlignVertical: TextAlignVertical(y: -0.5),
+                ),
               ),
             ),
           ),
         ),
       ]);
+
+  Widget _buildCarousel() {
+    return CarouselSlider.builder(
+      carouselController: _carouselController,
+      options: CarouselOptions(
+          height: 170.0,
+          enlargeCenterPage: true,
+          autoPlay: false,
+          aspectRatio: 16 / 9,
+          autoPlayCurve: Curves.fastOutSlowIn,
+          enableInfiniteScroll: true,
+          // autoPlayAnimationDuration: Duration(milliseconds: 800),
+          autoPlayInterval: Duration(seconds: 5),
+          viewportFraction: 0.60,
+          onPageChanged: (index, reason) {
+            setState(() {
+              _currentIndex = index;
+            });
+          }),
+      itemCount: 3,
+      itemBuilder: (context, index, realIndex) {
+        final opacity = (_currentIndex == index) ? 1.0 : 0.5;
+        return Opacity(
+          opacity: opacity,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image.asset(
+              'assets/imageHome$index.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+        );
+      },
+    );
+  }
 
   Widget buildHomeBody(BuildContext context) => Container(
         alignment: Alignment.center,
